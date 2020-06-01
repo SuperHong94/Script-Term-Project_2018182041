ce223108346b85d6bd912830535f05a6410ce293
SuperHong94 <50072558+SuperHong94@users.noreply.github.com>
2020-06-01 23:28:59 +0900

    프로젝트 시작
    ..

---
+++ UI.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+
+class MainGUI():
+    def __init__(self):
+        self.window=Tk()
+        self.window.title("경기도지역화페 가맹점 검색")
+        self.window.geometry("1200x600")
+        self.window.mainloop()
+
+
+
+
+MainGUI()
